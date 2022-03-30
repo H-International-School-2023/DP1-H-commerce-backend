ee03e6da56837fad6d9a303d155beda21e0a9c65
andreabeggiato-his <andrea.beggiato@h-is.com>
2022-03-30 12:48:26 +0200

    html

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world fadf asrefdashiufadhuirehiuwq jbnjb!</h1>');
+  const page = `
+    <html>
+      <head>
+        <title>
+          Home Page
+        </title>
+      </head>
+      <body>
+        <h1>
+          First Paragraph
+        </h1>
+        <p>
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+          This is the first paragraph
+        </p>
+        <p>
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+          This is the second paragraph
+        </p>
+      </body>
+    </html>
+  `;
+  res.send(page);
 });
 
-app.post('/login', (req, res) => {
-
+app.get('/hello', (req, res) => {
+  res.send('Another endpoint!')
 });
 
 app.listen(port);
