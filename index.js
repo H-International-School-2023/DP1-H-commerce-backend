3bc997209485e12b64df3bd659ea052dd41c4920
andreabeggiato-his <andrea.beggiato@h-is.com>
2022-04-11 10:34:17 +0200

    homepage ejs

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import express from 'express';
 import fs from 'fs';
 
-;
 const app = express();
 const port = 4000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
-const username = 'Andrea'
 const books = [
   'First',
   'Second',
   'Third',
   'Generic Tolkien Book'
 ];
+
+const users = [
+  { username: 'pippo', password: 'password' },
+  { username: 'andrea.beggiato', password: 'mypassword' },
+  { username: 'pluto', password: 'password' },
+];
+
+app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
   res.render('homepage', {
@@ -23,8 +31,23 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  console.log(req.body);
-  res.send('OK!');
+  const username = req.body.user;
+  const password = req.body.password;
+  let found = false;
+  for (let i = 0; i < users.length; i = i + 1) {
+    const user = users[i];
+    if (user.username === username && user.password === password) {
+      found = true;
+    }
+  }
+
+  if (found === true) {
+    res.send('You are authenticated!');
+  }
+  else {
+    res.send('Username not found');
+  }
+    
 })
 
 app.get('/hello', (req, res) => {
